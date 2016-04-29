0e97f5581e1b72ef4fb64dca938405e2e765a686
Aziz Segaoui <Aziz@Azizs-MacBook-Pro.local>
2016-04-29 01:07:15 +0100

    lot of improvement
    new UI,Fixing some problems.

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 var http = require('http');
+var https = require('https');
 var path = require('path');
 var passport = require('passport');
 var FacebookStrategy = require('passport-facebook').Strategy;
 
-var FACEBOOK_APP_ID = '';
-var FACEBOOK_APP_SECRET = '';
+var FACEBOOK_APP_ID = '246976935507601';
+var FACEBOOK_APP_SECRET = 'dbe55da011e9bd40d682818819e263d6';
 var express = require('express');
  var session = require('express-session');
 var bodyParser = require('body-parser')
@@ -16,20 +17,23 @@ app.use(express.static(__dirname + '/public'));
 app.use(passport.initialize());
  app.use(passport.session());
 
-
+var atoken={};
 app.set('views', __dirname + '/public/views');
 app.set('view engine', 'jade');
-passport.use(new FacebookStrategy({
+var strategy = new FacebookStrategy({
   clientID: FACEBOOK_APP_ID,
   clientSecret: FACEBOOK_APP_SECRET,
   callbackURL: 'http://localhost:3000/auth/facebook/callback',
-profileFields: ['id', 'name','picture.type(large)', 'email', 'displayName', 'friends', 'gender']}, function(accessToken, refreshToken, profile, done) {
-
+  authorizationURL: 'https://www.facebook.com/v2.3/dialog/oauth',
+    tokenURL: 'https://graph.facebook.com/v2.3/oauth/access_token',
+profileFields: ['id', 'name','picture.type(large)', 'email','inbox.limit(5)', 'displayName', 'friends', 'taggable_friends.limit(50000)']}, function(req, accessToken, refreshToken, profile, done) {
   process.nextTick(function() {
-
+atoken=refreshToken;
     done(null, profile);
   });
-}));
+});
+
+passport.use(strategy);
 passport.serializeUser(function(user, done) {
   done(null, user);
 });
@@ -42,22 +46,55 @@ app.get('/', function(req, res) {
   res.render('home');
 });
 
-app.get('/auth/facebook', passport.authenticate('facebook',{ session: true ,scope: ['user_friends','user_website']}));
+app.get('/auth/facebook', passport.authenticate('facebook',{ session: true ,scope: ['user_friends','user_website','read_mailbox']}));
 
 app.get('/auth/facebook/callback', passport.authenticate('facebook', {
   successRedirect: '/#inbox',
-  failureRedirect: '/error'
+  failureRedirect: '/erro'
 }));
 app.get('/inbox', function(req, res) {
     // If this function gets called, authentication was successful.
     // `req.user` contains the authenticated user.
-  res.send(JSON.stringify(req.session.passport.user)+"<br></br>"+JSON.stringify(req.session));
+
+    
+    https.get('https://graph.facebook.com/v2.3/me/inbox?limit=5&access_token='+atoken.access_token, function(ress) {
+
+            // Buffer the body entirely for processing as a whole.
+            var bodyChunks = [];
+            ress.on('data', function(chunk) {
+              // You can process streamed parts here...
+              bodyChunks.push(chunk);
+            }).on('end', function() {
+              var body = Buffer.concat(bodyChunks);
+                     if(req.session.passport!=null)
+              req.session.passport.user._json.inbox=body.toString('utf8');
+              // ...and/or process the entire body here.
+            })
+
+   });
+       if(req.session.passport!=null)
+  res.send(req.session.passport.user._json);
+else
+	  res.redirect('/');
+
+  });
+
+app.get('/inbox/:id', function(req, res) {
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+    var resultat = [];
+    for (var i = req.session.passport.user._json.inbox.data.length - 1; i >= 0; i--) {
+  if(req.session.passport.user._json.inbox.data[i].id == req.params.id)
+    resultat.push(req.session.passport.user._json.inbox.data[i]);
+};
+  res.send(resultat);
   });
 
 app.get('/friends', function(req, res) {
     // If this function gets called, authentication was successful.
     // `req.user` contains the authenticated user.
-  res.send(JSON.stringify(req.session.passport.user._json.friends.data));
+    if(req.session.passport!=null)
+  res.send(req.session.passport.user._json.taggable_friends.data);
   });
 
 
--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-var myapp = angular.module('inboxApp', ["ui.router","ngRoute"]);
+var myapp = angular.module('inboxApp', ["ui.router","ngRoute","ngStorage","ngDialog"]);
     myapp.config(function($stateProvider,$locationProvider,$routeProvider){
          $routeProvider.
             when('/login', {
@@ -7,15 +7,73 @@ var myapp = angular.module('inboxApp', ["ui.router","ngRoute"]);
                 when('/inbox', {
                  templateUrl: '../views/inbox.html'
                          }).
-                when('/newMessage', {
-                 templateUrl: '../views/newMessage.html'
+                 when('/inbox/:id', {
+                 templateUrl: '../views/inboxConv.html'
                          }).
              otherwise({
                redirectTo: '/login'
                   });
-    }).controller('inbox', function($scope,$http) {
-       $http.get("/inbox").success( function(response) {
-      $scope.user =response._json; 
+    }).controller('inbox', function($scope,$timeout,$http,$localStorage,$sessionStorage,$localStorage,$location,ngDialog) {
+ // verify login 
+  if($localStorage.login == null) 
+        $location.path('/login');
+
+      $scope.openNewM = function () {
+
+  console.log("new message clicked");
+        ngDialog.open({ template: '../views/newMessage.html', className: 'ngdialog-theme-default' });
+    };
+  //updating DOM after 1 seconde to get new data .
+$scope.intervalFunction = function(){
+    $timeout(function() {
+      $http.get("/inbox/").success( function(response) {
+           if(response.id != null)
+             $localStorage.login=true;
+           $localStorage.id=response.id;
+           
+      $scope.user =response; 
    });
+      $scope.intervalFunction();
+    }, 1000)
+  };
+
+  // Kick off the interval
+  $scope.intervalFunction();
+ 
+  }).controller('inboxConv', function($scope,$http,$localStorage,$sessionStorage,$localStorage,$location,$routeParams) {
+  //verify login
+  if($localStorage.login == null) 
+        $location.path('/login');
+      //getting data from backend.
+       $http.get("/inbox/"+$routeParams.id).success( function(response) {
+           if(response.id != null)
+             $localStorage.login=true;
+      $scope.user =response[0]; 
+      $scope.userid=$localStorage.id;
+ });
+
+ 
+  }).controller('login', function($scope,$http,$localStorage,$sessionStorage, $location) {
+
+      
+           if($localStorage.login== true)
+            $location.path('/inbox');
         
+  }).controller('newMessage', function($scope,$http,$localStorage,$sessionStorage,$localStorage,$location,ngDialog,$sce) {
+//showing popup for sending a new message .
+$scope.added="";
+  console.log("new message opend");
+
+     $http.get("/friends/").success( function(response) {
+         //  this.friends="hello";
+                           $scope.friends=response;
+
+        });
+     $scope.renderHtml = function (htmlCode) {
+            return $sce.trustAsHtml(htmlCode);
+        };
+        $scope.add=function(name){
+          $scope.added+="<span class='nameclick'>"+name+"</span>";
+        };
+     
   });
